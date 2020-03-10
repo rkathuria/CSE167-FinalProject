2492c8ef032d41e1f2e5e81625ca94c78a02e00a
Abi Sarvepalli <abisarvepalli@gmail.com>
2020-03-10 16:13:19 -0700

    upd gitignore; add files for shadow
    Signed-off-by: Abi Sarvepalli <abisarvepalli@gmail.com>

---
+++ CSE167FinalProject/CSE167FinalProject/shadows/ShadowMapper.cpp
@@ -0,0 +1,9 @@
+//
+//  ShadowMapper.cpp
+//  CSE167FinalProject
+//
+//  Created by Abinesh Sarvepalli on 3/10/20.
+//  Copyright © 2020 Raman Kathuria. All rights reserved.
+//
+
+#include "ShadowMapper.hpp"
---
+++ CSE167FinalProject/CSE167FinalProject/shadows/ShadowMapper.hpp
@@ -0,0 +1,14 @@
+//
+//  ShadowMapper.hpp
+//  CSE167FinalProject
+//
+//  Created by Abinesh Sarvepalli on 3/10/20.
+//  Copyright © 2020 Raman Kathuria. All rights reserved.
+//
+
+#ifndef ShadowMapper_hpp
+#define ShadowMapper_hpp
+
+#include <stdio.h>
+
+#endif /* ShadowMapper_hpp */
